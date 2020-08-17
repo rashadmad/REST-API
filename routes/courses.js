fe40ab63949ecc24a932d34b331e79e09fb3e9ea
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-17 11:07:20 -0700

    add GET method that displays multiple courses

--- routes/courses.js
+++ routes/courses.js
@@ -6,6 +6,12 @@ const asyncHelper = require('./asyncHandler');
 const Courses = require('../models').Courses;
 
 // GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
+router.get('/api/courses', asyncHelper.asyncHandler(async(req, res) => {
+    const arrayOfCourses = await Courses.findAll()
+    res.location('/')
+    res.send(arrayOfCourses)
+    res.status(200).end(); 
+}));
 // GET /api/courses/:id 200 - Returns a the course (including the user that owns the course) for the provided course ID
 // POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
 router.post('/api/courses', asyncHelper.asyncHandler(async(req, res) => {
