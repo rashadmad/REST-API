36255bd62f7eb9fcb3a00cd3925219a3bc723a80
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-23 19:08:48 -0700

    add valiadation to course creation

--- routes/courses.js
+++ routes/courses.js
@@ -19,10 +19,29 @@ router.get('/api/courses/:id', middleware.asyncHandler(async(req, res) => {
     res.status(200).end(); 
 }));
 // POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
-router.post('/api/courses',middleware.authenticateUser, middleware.asyncHandler(async(req, res) => {
-    const newCourse = await Courses.create(req.body);
-    res.location(req.url + "/" + newCourse.id); 
-    res.status(201).end(); 
+router.post('/api/courses', [
+    /*
+        title
+        description
+    */
+    check('title')
+        .exists()
+        .withMessage('Please provide a value for "title"'),
+    check('description')
+        .exists()
+        .withMessage('Please provide a value for "description"'),
+    ], middleware.authenticateUser, middleware.asyncHandler(async(req, res) => {
+    
+        const errors = validationResult(req);
+        
+        if (!errors.isEmpty()) {
+            const errorMessages = errors.array().map(error => error.msg);
+            res.status(400).json({ errors: errorMessages });
+        } else {
+            const newCourse = await Courses.create(req.body);
+            res.location(req.url + "/" + newCourse.id); 
+            res.status(201).end(); 
+        }    
 }));
 // PUT /api/courses/:id 204 - Updates a course and returns no content
 router.put('/api/courses/:id',middleware.authenticateUser, middleware.asyncHandler(async(req, res) => {
